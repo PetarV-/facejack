a20389f952808395561b2eddb87e28367d08a6a3
Petar Veličković <pv273@cam.ac.uk>
2017-01-28 16:59:43 +0000

    Finetuning function

--- fine-tune/fine_tuner.py
+++ fine-tune/fine_tuner.py
@@ -9,3 +9,16 @@
 """
 
 """
+
+from keras.models import Model
+from keras.callbacks import EarlyStopping
+
+def tune(mdl, X_train, Y_train, patience=5):
+    mdl.fit(X_train, Y_train,
+            batch_size=128,
+            nb_epoch=50,
+            verbose=1,
+            validation_split=0.1,
+            callbacks=[EarlyStopping(monitor='val_loss', patience=patience)],
+            shuffle=True)
+
