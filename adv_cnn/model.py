585f7cb3b7ca29819bc7ec927f6c47c5c5e17f6a
Petar Veličković <pv273@cam.ac.uk>
2017-01-29 05:44:08 +0000

    bugfix

--- adv_cnn/model.py
+++ adv_cnn/model.py
@@ -50,7 +50,7 @@ def get_model():
     #fc7 = Dropout(0.5)(fc7)
     fc7 = BatchNormalization()(fc7)
     logit = Dense(1, activation='linear', name='conf')(fc7)
-    out = Activation('sigmoid')
+    out = Activation('sigmoid')(logit)
 
     model = Model(input=inp, output=out)
 
